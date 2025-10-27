2c095e774b8794b92dfa14c14e242b74864b95c3
AzerPilavdzic <pilavdzic.azer@nsoft.com>
2025-10-27 11:33:29 +0100

    add mongo4 and mongo5 replica set

--- init/rs-init.js
+++ init/rs-init.js
@@ -7,7 +7,9 @@ const cfg = {
   members: [
     { _id: 0, host: "mongo1:27017", priority: 2 }, // prefer mongo1 as primary
     { _id: 1, host: "mongo2:27017", priority: 1 },
-    { _id: 2, host: "mongo3:27017", priority: 1 }
+    { _id: 2, host: "mongo3:27017", priority: 1 },
+    { _id: 3, host: "mongo4:27017", priority: 1 },
+    { _id: 4, host: "mongo5:27017", priority: 1 }
   ]
 };
 
